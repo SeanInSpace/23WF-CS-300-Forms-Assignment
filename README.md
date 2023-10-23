3b5f356a79379f0045caa87ea9bf4fd43fd5efe0
SeanInSpace <50900457+SeanInSpace@users.noreply.github.com>
2023-10-22 20:22:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 23WF-CS-300-Forms-Assignment
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/SeanInSpace/23WF-CS-300-Forms-Assignment)
---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Form</h1>
+    <form method="POST" action="/submit">
+      <label>First Name:</label>
+      <input type="text" name="firstName"><br><br>
+      
+      <label>Last Name:</label>  
+      <input type="text" name="lastName"><br><br>
+      
+      <label>Email:</label>
+      <input type="email" name="email"><br><br>
+      
+      <button type="submit">Submit</button>
+    </form>
+  `);
+});
+
+app.post('/submit', (req, res) => {
+  const { firstName, lastName, email } = req.body;
+
+  res.send(`
+    <h1>Form Data</h1>
+    <p>First Name: ${firstName}</p>
+    <p>Last Name: ${lastName}</p>
+    <p>Email: ${email}</p> 
+  `);
+});
+
+app.listen(3000);
